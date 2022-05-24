1700aedf2407aef3bb017c5535ed500a909771ce
rafidmahin20 <submanmahin@gmail.com>
2022-05-24 21:39:28 +0600

    edited

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ejaeyp.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
