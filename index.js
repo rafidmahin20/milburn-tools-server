1e72803cea6617f676b6db9d2c5fd74329579879
rafidmahin20 <submanmahin@gmail.com>
2022-05-26 00:47:34 +0600

    edited

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ async function run(){
             res.send(result);
         })
 
+        app.delete('/tool/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = toolCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.get('/user', verifyJWT, async(req, res) =>{
             const users = await userCollection.find().toArray();
             res.send(users);
