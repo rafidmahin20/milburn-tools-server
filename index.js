3e963f54ab11bc834483261213114cdead70c135
rafidmahin20 <submanmahin@gmail.com>
2022-05-25 22:01:07 +0600

    edited

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ async function run(){
             res.send(result);
         })
 
+        app.get('/user', async(req, res) =>{
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
         app.put('/user/:email', async(req, res) =>{
             const email = req.params.email;
             const user = req.body;
